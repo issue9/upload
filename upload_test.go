4e1ae6533a1b8d496e3a539afe7de83d8d4641c4
caixw <xnotepad@gmail.com>
2015-11-23 23:26:18 +0800

    修正测试错误，更新示例内容

--- upload_test.go
+++ upload_test.go
@@ -14,21 +14,21 @@ import (
 func TestNew(t *testing.T) {
 	a := assert.New(t)
 
-	u, err := New("./testdir", 10*1024, "gif", ".png", ".GIF")
+	u, err := New("./testdir", "2006/01/02/", 10*1024, "gif", ".png", ".GIF")
 	a.NotError(err).NotNil(u)
 	// 自动转换成小写，且加上最前面的.符号
 	a.Equal(u.exts, []string{".gif", ".png", ".gif"})
 	a.Equal(u.dir, "./testdir"+string(os.PathSeparator))
 
 	// dir为一个文件
-	u, err = New("./testdir/file", 10*1024, "gif", ".png", ".GIF")
+	u, err = New("./testdir/file", "2006/01/02/", 10*1024, "gif", ".png", ".GIF")
 	a.Error(err).Nil(u)
 }
 
 func TestUpload_isAllowExt(t *testing.T) {
 	a := assert.New(t)
 
-	u, err := New("./testdir", 10*1024, "gif", ".png", ".GIF")
+	u, err := New("./testdir", "2006/01/02/", 10*1024, "gif", ".png", ".GIF")
 	a.NotError(err).NotNil(u)
 	a.True(u.isAllowExt(".gif"))
 	a.True(u.isAllowExt(".png"))
@@ -40,7 +40,7 @@ func TestUpload_isAllowExt(t *testing.T) {
 func TestUpload_getDestPath(t *testing.T) {
 	a := assert.New(t)
 
-	u, err := New("./testdir", 10*1024, "gif", ".png", ".GIF")
+	u, err := New("./testdir", "2006/01/02/", 10*1024, "gif", ".png", ".GIF")
 	a.NotError(err).NotNil(u)
 
 	t.Log(u.getDestPath(".png"))
